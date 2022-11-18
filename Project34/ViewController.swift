469a68bdbe10e66837b450e41b62f44e68a5767e
Grant Watson <gaw922@gmail.com>
2022-11-18 16:11:06 -0600

    Initial Commit

---
+++ Project34/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project34
+//
+//  Created by Grant Watson on 11/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var columnButtons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func makeMove(_ sender: Any) {
+    }
+    
+}
+
