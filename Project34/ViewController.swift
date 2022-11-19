d065ab7f090115b79cb3fd3cd149ddbb3c45a6c4
Grant Watson <gaw922@gmail.com>
2022-11-19 12:33:00 -0600

    Project34

--- Project34/ViewController.swift
+++ Project34/ViewController.swift
@@ -36,8 +36,47 @@ class ViewController: UIViewController {
         }
     }
 
-    @IBAction func makeMove(_ sender: Any) {
+    
+    @IBAction func makeMove(_ sender: UIButton) {
+        let column = sender.tag
+        
+        if let row = board.nextEmptySlot(in: column) {
+            board.add(chip: .red, in: column)
+            addChip(inColumn: column, row: row, color: .red)
+        }
     }
     
+    func addChip(inColumn column: Int, row: Int, color: UIColor) {
+        let button = columnButtons[column]
+        let size = min(button.frame.width, button.frame.height / 6)
+        let rect = CGRect(x: 0, y: 0, width: size, height: size)
+        
+        if (placedChips[column].count < row + 1) {
+            let newChip = UIView()
+            newChip.frame = rect
+            newChip.isUserInteractionEnabled = false
+            newChip.backgroundColor = color
+            newChip.layer.cornerRadius = size / 2
+            newChip.center = positionForChip(inColumn: column, row: row)
+            newChip.transform = CGAffineTransform(translationX: 0, y: -800)
+            view.addSubview(newChip)
+            
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseIn) {
+                newChip.transform = CGAffineTransform.identity
+            }
+            
+            placedChips[column].append(newChip)
+        }
+    }
+    
+    func positionForChip(inColumn column: Int, row: Int) -> CGPoint {
+        let button = columnButtons[column]
+        let size = min(button.frame.width, button.frame.height / 6)
+        
+        let xOffset = button.frame.midX
+        var yOffset = button.frame.maxY - size / 2
+        yOffset -= size * CGFloat(row)
+        return CGPoint(x: xOffset, y: yOffset)
+    }
 }
 
